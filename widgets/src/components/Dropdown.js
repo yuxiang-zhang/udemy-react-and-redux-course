cb4ee4459cc52793061357765bb481376944e452
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 17:26:24 -0400

    remove eventlistener when dropdown is removed

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,52 +1,54 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, {useState, useEffect, useRef} from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
-  const [open, setOpen] = useState(false)
-  const ref = useRef()
+const Dropdown = ({options, selected, onSelectedChange}) => {
+  const [open, setOpen] = useState (false);
+  const ref = useRef ();
 
-  useEffect(() => {
-    document.body.addEventListener(
-      'click',
-      e => {
-        if (!ref.current.contains(e.target)) setOpen(false)
-      },
-      { capture: true }
-    )
-  }, [])
+  useEffect (() => {
+    const onBodyClick = e => {
+      if (!ref.current.contains (e.target)) setOpen (false);
+    };
 
-  const renderedOptions = options.map(option => {
+    document.body.addEventListener ('click', onBodyClick, {capture: true});
+
+    return () => {
+      document.body.removeEventListener ('click', onBodyClick, {capture: true});
+    };
+  }, []);
+
+  const renderedOptions = options.map (option => {
     if (option.value === selected.value) {
-      return null
+      return null;
     }
 
     return (
       <div
         key={option.value}
-        className='item'
-        onClick={() => onSelectedChange(option)}
+        className="item"
+        onClick={() => onSelectedChange (option)}
       >
         {option.label}
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <div ref={ref} className='ui form'>
-      <div className='field'>
-        <label className='label'>Select a Color</label>
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => setOpen (!open)}
           className={`ui selection dropdown ${open && 'visible active'}`}
         >
-          <i className='dropdown icon'></i>
-          <div className='text'>{selected.label}</div>
+          <i className="dropdown icon" />
+          <div className="text">{selected.label}</div>
           <div className={`menu ${open && 'visible transition'}`}>
             {renderedOptions}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
