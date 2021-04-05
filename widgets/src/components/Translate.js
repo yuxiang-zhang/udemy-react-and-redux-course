3a71901c539b4d044ff97e4ae39d760042c0bca2
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 18:16:38 -0400

    add convert to translate component

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -34,6 +35,9 @@ const Translate = () => {
         onSelectedChange={setLang}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert lang={lang} text={text} />
     </div>
   );
 };
