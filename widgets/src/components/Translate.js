b09700e6423ec3a1d498120d5938d2a62de9c4d7
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 17:43:50 -0400

    init translate component

---
+++ widgets/src/components/Translate.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Dropdown from './Dropdown';
+
+const Translate = () => {
+  return (
+    <div>
+      <Dropdown />
+    </div>
+  );
+};
+
+export default Translate;
