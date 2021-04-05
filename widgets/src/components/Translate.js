09f13b2c388bbec8f521b8fbf3eaa271e0da2769
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 17:45:45 -0400

    add language options

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,6 +1,21 @@
 import React, {useState} from 'react';
 import Dropdown from './Dropdown';
 
+const options = [
+  {
+    label: 'Afrikaans',
+    value: 'af',
+  },
+  {
+    label: 'Arabic',
+    value: 'ar',
+  },
+  {
+    label: 'Hindi',
+    value: 'hi',
+  },
+];
+
 const Translate = () => {
   return (
     <div>
