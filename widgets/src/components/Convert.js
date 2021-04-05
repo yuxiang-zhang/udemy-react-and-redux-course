225bec59f3d71608fcda20a1dc5964b806c6a30f
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 18:55:16 -0400

    use axios to send request to translate api

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,7 +1,23 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Convert = ({lang, text}) => {
-  useEffect (() => {}, [lang, text]);
+  useEffect (
+    () => {
+      axios.post (
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: lang.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+    },
+    [lang, text]
+  );
 
   return <div />;
 };
