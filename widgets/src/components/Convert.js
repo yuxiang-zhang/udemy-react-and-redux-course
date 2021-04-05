9e94b5edd2612410fdbdb9375b5373dd365ec0e7
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 19:02:27 -0400

    add translated state; set translated state from api result

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,24 +2,34 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Convert = ({lang, text}) => {
+  const [translated, setTranslated] = useState ('');
+
   useEffect (
     () => {
-      axios.post (
-        'https://translation.googleapis.com/language/translate/v2',
-        {},
-        {
-          params: {
-            q: text,
-            target: lang.value,
-            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-          },
-        }
-      );
+      (async () => {
+        const {data: {data: {translations}}} = await axios.post (
+          'https://translation.googleapis.com/language/translate/v2',
+          {},
+          {
+            params: {
+              q: text,
+              target: lang.value,
+              key: '',
+            },
+          }
+        );
+
+        setTranslated (translations[0].translatedText);
+      }) ();
     },
     [lang, text]
   );
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
