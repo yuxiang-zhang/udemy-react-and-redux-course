d39f5c3de23ae10470927d328052a218a75ed4e3
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 17:57:56 -0400

    add text field

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -18,9 +18,16 @@ const options = [
 
 const Translate = () => {
   const [lang, setLang] = useState (options[0]);
+  const [text, setText] = useState ('');
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={e => setText (e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         label="Select A Language"
         selected={lang}
